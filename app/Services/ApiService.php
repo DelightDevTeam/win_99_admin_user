ea4d51464422234abb98f94590d4742c75b7289d
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-07-04 12:23:08 +0630

    repair

--- app/Services/ApiService.php
+++ app/Services/ApiService.php
@@ -6,20 +6,61 @@ use Illuminate\Support\Facades\Http;
 
 class ApiService
 {
-    protected $baseUrl;
+    // protected $baseUrl;
 
-    public function __construct($baseUrl)
+    // public function __construct($baseUrl)
+    // {
+    //     $this->baseUrl = $baseUrl;
+    // }
+
+    // public function get($endpoint, $params = [])
+    // {
+
+    //     $url = $this->baseUrl.$endpoint;
+
+    //     $response = Http::get($url, $params);
+
+    //     return $response->json();
+    // }
+
+     protected $baseUrl;
+
+    public function __construct()
     {
-        $this->baseUrl = $baseUrl;
+        $this->baseUrl = config('game.api.url');
     }
 
-    public function get($endpoint, $params = [])
+    public function get($endpoint, $params = [], $maxRetries = 5)
     {
+        return $this->makeRequest('get', $endpoint, $params, $maxRetries);
+    }
 
-        $url = $this->baseUrl.$endpoint;
+    public function post($endpoint, $data = [], $maxRetries = 5)
+    {
+        return $this->makeRequest('post', $endpoint, $data, $maxRetries);
+    }
 
-        $response = Http::get($url, $params);
+    protected function makeRequest($method, $endpoint, $data, $maxRetries)
+    {
+        $attempt = 0;
+        $backoff = 1;
+        $url = $this->baseUrl . $endpoint;
 
-        return $response->json();
+        while ($attempt < $maxRetries) {
+            $response = Http::$method($url, $data);
+
+            if ($response->status() == 429) {
+                sleep($backoff);
+                $backoff *= 2;
+                $attempt++;
+            } else {
+                return $response->json();
+            }
+        }
+
+        return [
+            'status_code' => 429,
+            'error' => 'Too many requests',
+        ];
     }
 }
